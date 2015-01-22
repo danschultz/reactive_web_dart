3fbb4be7f10d2a4e663ce35ee66f76ccc40e5e29
Dan Schultz <schultz.t.dan@gmail.com>
2015-01-22 13:47:04 -0800

    Document model updating behavior

--- lib/src/model_controller/model_controller.dart
+++ lib/src/model_controller/model_controller.dart
@@ -7,6 +7,11 @@ class ModelController<T> {
   Stream<T> get model => _model;
 
   ModelController(T initial) {
+    // Updates the model using a "fold" behavior. Whenever an update action is observed, `Scan`
+    // will use an accumulator function that is passed a copy of the previous model and the
+    // action to perform on it. The action will be invoked with the previous model and returns
+    // a version of the model with the action's changes. The updated copy of the model will then
+    // be pushed out to any observers of the `model` stream.
     _model = _updates.stream.transform(new Scan(initial, (T model, Action<T> action) => action(model)));
   }
 
